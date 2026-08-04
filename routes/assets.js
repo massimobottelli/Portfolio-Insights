c276cde79b0f55b0990814505850510531b74f33
Massimo Bottelli <88494940+massimobottelli@users.noreply.github.com>
2026-08-04 17:02:49 +0200

    Delete routes directory

--- routes/assets.js
+++
@@ -1,8 +0,0 @@
-import { router } from '../router.js';
-import { getAssets } from '../database.js';
-
-router.get('/api/assets', (req, res) => {
-  const assets = getAssets();
-  res.writeHead(200, { 'Content-Type': 'application/json' });
-  res.end(JSON.stringify(assets));
-});
--- routes/imports.js
+++
@@ -1,7 +0,0 @@
-import { router } from '../router.js';
-
-router.post('/api/import', async (req, res) => {
-  // Qui inseriremo la logica del parser Directa
-  res.writeHead(200, { 'Content-Type': 'application/json' });
-  res.end(JSON.stringify({ success: true, message: 'File importato con successo!' }));
-});
